6578d7e0fb2c6631d41b21a1363ec6cd07cebb27
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-06-17 19:22:44 +0530

    add new files

--- Assignment2.js
+++ Assignment2.js
@@ -1,7 +1,16 @@
 // Write an if/else statement that checks if a number is even or odd. If it's even, print "The number is even." Otherwise, print "The number is odd."
 
-function EvenNum {
+function EvenNum (num) {
 
 
-if (num =)    
+if (  num%2 == 0 ) {
+console.log("Eveennnnnnnnnnnn")  
 }
+else {
+    console.log("Oddddddd")
+    // return false;
+}}
+
+let result =EvenNum (67);
+// return EvenNum;
+console.log (result);
