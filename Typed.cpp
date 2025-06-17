95a14fc3f7a2423c3dda1b8465e86f17f7b12976
Khushiii👩‍💻 <145993686+khushikumari239@users.noreply.github.com>
2025-06-17 07:37:47 +0530

    add new files

---
+++ Typed.cpp
@@ -0,0 +1,10 @@
+// C++ Code (won’t compile)
+
+
+#include <iostream>
+
+int main() { 
+  int a = 1;
+  a = "hello";
+  a = true;
+}
---
+++ Typed.js
@@ -0,0 +1,6 @@
+// Variables in JavaScript are not bound to a specific data type. Types are determined at runtime and can change as the program executes
+var a = 1;
+a = "harkirat";
+a = true;
+
+console.log(a)
